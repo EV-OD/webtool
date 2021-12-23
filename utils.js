f322f284bc32f6563f83c72bc34f12f5b04c26ec
EV-OD <evod599@gmail.com>
2021-12-23 21:26:54 +0545

    bug fixed

--- utils.js
+++ utils.js
@@ -16,7 +16,7 @@ function setCode(code){
 
 function runCode(){
     let r = `${editorHtml.getValue()} 
-        <style defer>${editorCss.getValue()}</style>
-        <script>${editorJs.getValue()}</script>`
+        <style>${editorCss.getValue()}</style>
+        <script defer>${editorJs.getValue()}</script>`
     setCode(r)
 }
